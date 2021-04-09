5297caa4dd76f67aee4725357306d51d733e6b96
Leevi Ossi <leevi.ossi@hotmail.com>
2021-04-09 17:00:48 +0300

    exercises 4.1-4.2

---
+++ osa4/models/blog.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+const config = require('../utils/config')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Blog', blogSchema)
