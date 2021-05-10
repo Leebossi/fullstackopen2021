5c8dc1d00af54397dc2e6ab1365e4cb01048215b
Leevi Ossi <leevi.ossi@hotmail.com>
2021-05-10 15:01:16 +0300

    exercise 7.17

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,69 +1,88 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const Comment = require('../models/comment')
 const { userExtractor } = require('../utils/middleware')
 
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog
-        .find({}).populate('user', { username: 1, name: 1 })
-    response.json(blogs.map(blog => blog.toJSON()))
+  const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1 })
+    .populate('comments', { content: 1 })
+  response.json(blogs.map(blog => blog.toJSON()))
 })
 
 blogsRouter.post('/', userExtractor, async (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const user = request.user
+  const user = request.user
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes,
-        user: user._id
-    })
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user._id
+  })
 
-    const savedBlog = await blog.save()
-    user.blogs = user.blogs.concat(savedBlog._id)
-    await user.save()
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
 
-    response.json(savedBlog.toJSON())
+  response.json(savedBlog.toJSON())
 })
 
 blogsRouter.get('/:id', async (request, response, next) => {
-    const blog = await Blog.findById(request.params.id)
-    if (blog) {
-        response.json(blog.toJSON())
-    } else {
-        response.status(404).end()
-    }
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    response.json(blog.toJSON())
+  } else {
+    response.status(404).end()
+  }
 })
 
 blogsRouter.delete('/:id', userExtractor, async (request, response) => {
-    const user = request.user
-    const blog = await Blog.findById(request.params.id)
+  const user = request.user
+  const blog = await Blog.findById(request.params.id)
 
-    if(!blog) {
-        return response.status(404).end()
-    } else if (user.id.toString() === blog.user.toString()) {
-        await Blog.findByIdAndRemove(request.params.id)
-        response.status(204).end()
-    }
+  if (!blog) {
+    return response.status(404).end()
+  } else if (user.id.toString() === blog.user.toString()) {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  }
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
-    const body = request.body
+  const body = request.body
 
-    const blog = {
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-    }
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
 
-    Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-        .then(updatedBlog => {
-            response.json(updatedBlog.toJSON())
-        })
+  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    .then(updatedBlog => {
+      response.json(updatedBlog.toJSON())
+    })
+})
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const body = request.body
+  const comment = new Comment({
+    content: body.content,
+    blog: request.params.id
+  })
+
+  const blog = await Blog.findById(request.params.id)
+
+  const savedComment = await comment.save()
+  
+  blog.comments = blog.comments.concat(savedComment)
+
+  await blog.save()
+  response.status(201).json(savedComment)
 })
 
 module.exports = blogsRouter
--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -5,31 +5,37 @@ const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const blogSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true,
-    },
-    author: String,
-    url: {
-        type: String,
-        required: true,
-    },
-    likes: {
-        type: Number,
-        default: 0,
-    },
-    user: {
-        type: mongoose.Schema.ObjectId,
-        ref: 'User'
+  title: {
+    type: String,
+    required: true,
+  },
+  author: String,
+  url: {
+    type: String,
+    required: true,
+  },
+  likes: {
+    type: Number,
+    default: 0,
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User'
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Comment'
     }
+  ]
 })
 
 blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Blog', blogSchema)
