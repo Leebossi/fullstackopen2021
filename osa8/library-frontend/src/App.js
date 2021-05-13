485ce8bd630082950ff71a06e0907585efa52f6d
Leevi Ossi <leevi.ossi@hotmail.com>
2021-05-13 11:43:06 +0300

    exercise 8.8

---
+++ osa8/library-frontend/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const ALL_AUTHORS = gql `
+  query {
+    allAuthors {
+      name
+      born
+      bookCount
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(ALL_AUTHORS)
+  const [page, setPage] = useState('authors')
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors
+        show={page === 'authors'}
+        authors={result.data.allAuthors}
+      />
+
+      <Books
+        show={page === 'books'}
+      />
+
+      <NewBook
+        show={page === 'add'}
+      />
+
+    </div>
+  )
+}
+
+export default App
