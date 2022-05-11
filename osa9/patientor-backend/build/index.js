fa6eb8545cb9bb2f2eb863bce78ffc38954873b0
Leevi Ossi <leevi.ossi@hotmail.com>
2022-05-11 16:13:03 +0300

    Exercise 9.9

--- osa9/patientor-backend/build/index.js
+++ osa9/patientor-backend/build/index.js
@@ -4,9 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const PORT = 3000;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use((0, cors_1.default)());
+const PORT = 3001;
 app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
