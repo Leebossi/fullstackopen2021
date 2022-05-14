fecf27d0f2a7cd9ce2f00472e69c9c4353fce997
Leevi Ossi <leevi.ossi@hotmail.com>
2022-05-14 17:17:52 +0300

    Exercise 9.12

--- osa9/patientor-backend/src/routes/patients.ts
+++ osa9/patientor-backend/src/routes/patients.ts
@@ -1,10 +1,23 @@
 import express from "express";
-import patientService from '../services/patientService';
+import patientService from "../services/patientService";
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 
 const router = express.Router();
 
 router.get("/", (_req, res) => {
   res.send(patientService.getNonSensitivePatients());
+});
+
+router.post("/", (req, res) => {
+  const { name, gender, occupation, dateOfBirth, ssn } = req.body;
+  const newPatient = patientService.addPatient({
+    name,
+    gender,
+    occupation,
+    dateOfBirth,
+    ssn
+  });
+  res.json(newPatient);
 });
 
 export default router;
--- osa9/patientor-backend/src/services/patientService.ts
+++ osa9/patientor-backend/src/services/patientService.ts
@@ -1,5 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import patientData from "../../data/patients.json";
-import { Patient, NonSensitivePatient } from "../types";
+import { v1 as uuid } from 'uuid';
+import { Patient, NonSensitivePatient, NewPatient } from "../types";
 
 const getPatients = (): Patient[] => {
   return patientData;
@@ -15,7 +18,19 @@ const getNonSensitivePatients = (): NonSensitivePatient[] => {
   }));
 };
 
+const addPatient = (patient: NewPatient): Patient => {
+  const id = uuid();
+  const newPatient = {
+    id,
+    ...patient
+  };
+
+  patientData.push(newPatient);
+  return newPatient;
+};
+
 export default {
   getPatients,
   getNonSensitivePatients,
+  addPatient
 };
