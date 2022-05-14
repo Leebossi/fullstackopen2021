0e0994cd27302b8b43479d9b471cf09c24cccf7d
Leevi Ossi <leevi.ossi@hotmail.com>
2022-05-14 16:20:20 +0300

    Exercise 9.10

---
+++ osa9/patientor-backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import diagnoseRouter from "./routes/diagnoses";
+
+const app = express();
+app.use(express.json());
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors());
+
+const PORT = 3001;
+
+app.get("/api/ping", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
+
+app.use('/api/diagnoses', diagnoseRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ osa9/patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,10 @@
+import diagnoseData from '../../data/diagnoses.json';
+import { Diagnose } from '../types';
+
+const diagnoses: Array<Diagnose> = diagnoseData;
+
+const getDiagnoses = (): Array<Diagnose> => {
+  return diagnoses;
+};
+
+export default getDiagnoses;
